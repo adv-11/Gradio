3cc9a5627399bd3820428de78959e8820634454d
adv-11 <advaitss11@gmail.com>
2024-11-16 22:39:54 +0530

    components

--- app.py
+++ app.py
@@ -3,10 +3,32 @@ import gradio as gr
 def add_numbers(x,y):
     return x+y 
 
-interface = gr.Interface(fn=add_numbers , 
-                         inputs=[gr.Number(10) , gr.Number()],
-                         outputs= gr.Number()
-                         )
+def reverse_text(input_text):
+    return input_text[::-1]
+
+def slider (value):
+    return 'Current Value is {value}'
+
+options = ['A', 'B' , 'C']
+
+def dropDown(value):
+    return "Value you selected is ",value ,"."
+
+
+from PIL import Image 
+
+def convert (image_path):
+    image = Image.open(image_path)
+    gray_image = image.convert("L")
+    return gray_image
+
+#json
+
+#label 
+
+interface = gr.Interface(fn=convert , 
+                         inputs=[gr.Image(type='filepath')],
+                         outputs= gr.Image()
+                        )
 
 interface.launch()
-
